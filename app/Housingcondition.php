aa5f899f6f35ed573c73cf82b4559110f6b5f76b
momokipf <momokipf@gmail.com>
2017-06-22 10:51:16 -0700

    fix name's bug

--- app/Housingcondition.php
+++ app/Housingcondition.php
@@ -11,7 +11,7 @@ class Housingcondition extends Model
     *
     * @var string
     */
-    protected $table = 'Housingcondition';
+    protected $table = 'HousingCondition';
     protected $hidden = [];
      /**
      * The attributes that are mass assignable.
--- app/Http/Controllers/HouseOwnersController.php
+++ app/Http/Controllers/HouseOwnersController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
-use App\HouseOwner;
+use App\Houseowner;
 
 
 class HouseOwnersController extends Controller
