e5e0a1104bbdf272109f896842e8488a3f4422fe
YYC <yaoyichen94@gmail.com>
2017-07-28 15:25:56 -0700

    add view all transaction page & modify transaction model

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -7,11 +7,15 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
 
+use App\Transaction;
+
 class TransactionsController extends Controller
 {
     public function show(Request $request)
     {
+    	$tran = Transaction::all();
         return view('transaction.showAll')
-                ->with('Rep',Auth::user());
+                ->with('Rep', Auth::user())
+                ->with('tran', $tran);
     }
 }
--- resources/views/transaction/ShowAll.blade.php
+++ resources/views/transaction/ShowAll.blade.php
@@ -1,15 +1,52 @@
 @extends('navbar')
-@section('title', 'Show All Inquirers')
+@section('title', 'Show All Transactions')
 
 @section('head')
 	<style>
 		
 	</style>
+
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css"/>
+
+	<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
+	<script type="text/javascript" src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$('#transactionTable').DataTable({
+				"columnDefs": [
+					{
+						"targets": [ 5 ],
+						"sortable": false
+					}
+				]
+			});
+		});
+	</script>
 @endsection
 
 @section('content')
 	<div class="container">
-		
+		<table id="transactionTable" class="table table-hover">
+			<thead>
+				<th>Transaction ID</th><th>Inquirer Name</th><th>Full House ID</th><th>Checkin Date</th><th>Checkout Date</th><th>Amount</th><th></th>
+			</thead>
+			<tbody>
+				@foreach ($tran as $tr)
+					<tr>
+						<td>{{$tr->transactionID}}</td>
+						<td>{{$tr->inquiry->quirer->inquirerFirst}}</td>
+						<td>{{$tr->house->fullHouseID}}</td>
+						<td>{{$tr->inquiry->checkIn}}</td>
+						<td>{{$tr->inquiry->checkOut}}</td>
+						<td>{{$tr->amount}}</td>
+						<td align='right' style='padding-right:20px'>
+							<button type="button" class="btn btn-primary">View Details</button>
+						</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
 	</div>
 @endsection
 
